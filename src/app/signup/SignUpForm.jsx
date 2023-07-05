c5f35e37224ced981b2329b3052df9f86e250823
Daniel Lancheros <daniellancheros@Daniels-MacBook-Air.local>
2023-07-05 14:40:47 -0400

    signup with email and password working

--- src/app/signup/SignUpForm.jsx
+++ src/app/signup/SignUpForm.jsx
@@ -1,9 +1,34 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { AuthContext } from "../../App";
+const firebaseConfig = {
+    apiKey: "AIzaSyBaIlwOTwZM1dTXdyWiRSBFkk_tUwFuEJA",
+    authDomain: "chekov-dl.firebaseapp.com",
+    projectId: "chekov-dl",
+    storageBucket: "chekov-dl.appspot.com",
+    messagingSenderId: "148888952125",
+    appId: "1:148888952125:web:ee12096ff5df55a6683954"
+  };
+
 export default function SignUpForm(){
+    
+    const { setUser } = useContext(AuthContext);
+    const navigate = useNavigate();
 
     const handleSignup = (e) => {
-        e.preventdefault();
+        e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
+        const app = initializeApp(firebaseConfig);
+        const auth = getAuth(app);
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(response => {
+                setUser(response.user);
+                navigate("/");
+    })
+    .catch(err => alert(err.message))
     }
     return (
         <form onSubmit={handleSignup}>
@@ -17,7 +42,7 @@ export default function SignUpForm(){
             <input type="password" name="password" />
         </label>
         <br />
-        <input type = "submit" value = "Sign Up" />
+        <input type="submit" value="Sign Up" />
         </form>
     )
 }
--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import SignUpForm from "./SignUpForm"
 
 export default function SignUp() {
@@ -5,6 +6,7 @@ export default function SignUp() {
         <main>
             <h1>Sign Up</h1>
             <SignUpForm/>
+            Already a user? <Link to="/login">Login</Link>
         </main>
     )
 }
